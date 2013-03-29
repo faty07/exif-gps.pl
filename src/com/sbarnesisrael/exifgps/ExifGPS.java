3ee493b4c17cc1d5b5c91a6c2d4f283972f6f6f4
Spencer Barnes-Israel <s.barnes.israel@gmail.com>
2013-03-29 15:20:15 -0400

    Refactored the code. convertCoords method was created to allow for more flexible code in the future. No need to pass both Longitude and Latitude at the same time. You pass them separately to convert to coords pair.

--- src/com/sbarnesisrael/exifgps/ExifGPS.java
+++ src/com/sbarnesisrael/exifgps/ExifGPS.java
@@ -19,7 +19,7 @@ public class ExifGPS {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new ExifGPS("/home/sbarnesisrael/Downloads/exif_java/res/41137_2_279_123_256.jpg");
+		new ExifGPS("/home/sbarnesisrael/Downloads/exif_java/res/f6db4c02e353bc82cfb2d5fd277d76cf.jpg");
 	}
 	
 	public ExifGPS(String fileName) {
@@ -45,7 +45,7 @@ public class ExifGPS {
             System.out.println("Model: " + model);
             System.out.println("Latitude: " + latitude + " " + latitudeRef);
             System.out.println("Longitude: " + longitude + " " + longitudeRef);
-            System.out.println("LatLong Pair: " + getLongLatPair(latitude, longitude));
+            System.out.println("LatLong Pair: " + convertCoords(latitude) + "," + convertCoords(longitude));
             
         } catch (ImageProcessingException e) {
             System.err.println("error 1a: " + e);
@@ -73,38 +73,31 @@ public class ExifGPS {
 		return Double.valueOf(twoDForm.format(d));
 	}
 	
-	public String getLongLatPair(String latitude, String longitude){
+	public String convertCoords(String coords){
 		// TODO: Refactor code to run regardless of Lat or Long (one method, run twice - once per coord required)
 		
 		// Grab just what we need
 		// NOTE: \u00b0 is the degrees symbol in Unicode
-		String latDegrees = latitude.substring(0,latitude.indexOf("\u00b0"));
-		String latMinutes = latitude.substring(latitude.indexOf("\u00b0") + 1,latitude.indexOf("'"));
-		String latSeconds = latitude.substring(latitude.indexOf("'") + 1,latitude.indexOf("\""));
-		String longDegrees = longitude.substring(0,longitude.indexOf("\u00b0"));
-		String longMinutes = longitude.substring(longitude.indexOf("\u00b0") + 1,longitude.indexOf("'"));
-		String longSeconds = longitude.substring(longitude.indexOf("'") + 1,longitude.indexOf("\""));
-		
+		String degCoords = coords.substring(0,coords.indexOf("\u00b0"));
+		String minCoords = coords.substring(coords.indexOf("\u00b0") + 1,coords.indexOf("'"));
+		String secCoords = coords.substring(coords.indexOf("'") + 1,coords.indexOf("\""));
+				
 		// Do the conversion math to get a LatLong Pair
 		// If the degrees are negative we need to be sure we are not adding to a negative number
-		double cnvrtdLat = 0;
-		if (Double.parseDouble(latDegrees) <= 0) {
+		double cnvrtdCoords = 0;
+		if (Double.parseDouble(degCoords) <= 0) {
 			// West or South
-			cnvrtdLat = -(Math.abs(Double.parseDouble(latDegrees)) + (Double.parseDouble(latMinutes)/60) + (Double.parseDouble(latSeconds)/3600));
+			cnvrtdCoords = -(Math.abs(Double.parseDouble(degCoords)) + (Double.parseDouble(minCoords)/60) + (Double.parseDouble(secCoords)/3600));
 		} else {
 			// East or North
-			cnvrtdLat = Double.parseDouble(latDegrees) + (Double.parseDouble(latMinutes)/60) + (Double.parseDouble(latSeconds)/3600);
+			cnvrtdCoords = Double.parseDouble(degCoords) + (Double.parseDouble(minCoords)/60) + (Double.parseDouble(secCoords)/3600);
 		}
-		
-		double cnvrtdLong = Math.abs(Double.parseDouble(longDegrees)) + (Double.parseDouble(longMinutes)/60) + (Double.parseDouble(longSeconds)/3600);
-		
+				
 		//Round to 4 decimal places
-		cnvrtdLat = roundFourDecimals(cnvrtdLat);
-		cnvrtdLong = roundFourDecimals(cnvrtdLong);
+		cnvrtdCoords = roundFourDecimals(cnvrtdCoords);
 		
-		// Convert everything to a string and return
-		String longLatPair = Double.toString(cnvrtdLat) + "," + Double.toString(cnvrtdLong);	
-		return longLatPair;
+		// Return what we found
+		return Double.toString(cnvrtdCoords);
 	}
 
 }
